32297511ca8431ebe64c560b276f16c0ab2aca1d
nadinekhis <torontino123@gmail.com>
2020-10-21 18:58:26 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let res = (Array.from(n.toString())).reverse();
+    if (res[res.length -1] === '-')
+        res.pop()
+    return res.join('');
 }
